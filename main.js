fbefc0c2fc231a690d0bc11dee79183455f13f16
Narender <chinna.govindula>
2023-12-07 14:37:24 +0530

    New chnages

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings: ["Angular Developer", "Software Developer", "Frontend Developer", "Web Developer", "Graphic Designer", "Video Editor"],
+    strings: ["Angular Developer!", "Software Developer!", "Frontend Developer!", "Web Developer!", "Graphic Designer!", "Video Editor!"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
